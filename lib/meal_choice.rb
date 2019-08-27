d1e72b48c89f13e13abccf5eb841aa0646240017
virtual-frame-7789 <mttwright@gmail.com>
2019-08-27 00:44:14 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -9,6 +9,8 @@ def meal_choice(veg1, veg2, protein="meat")
   puts sentence1
   puts sentence2
   
+  "A plate of #{protein} with #{veg1} and #{veg2}."
+  
 end
 
 meal_choice("broccoli", "macaroni", "tofu")
