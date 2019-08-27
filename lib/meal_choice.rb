82119f0ff3b4692cb5e795ef4efd54a7a4501dd2
virtual-frame-7789 <mttwright@gmail.com>
2019-08-27 00:45:03 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,16 @@
 # Your code here
+
+def meal_choice(veg1, veg2, protein="meat")
+  
+  sentence1 = "What a nutritious meal!"
+  sentence2 = "A plate of #{protein} with #{veg1} and #{veg2}."
+  
+  
+  puts sentence1
+  puts sentence2
+  
+  "A plate of #{protein} with #{veg1} and #{veg2}."
+  
+end
+
+meal_choice("broccoli", "macaroni", "tofu")
